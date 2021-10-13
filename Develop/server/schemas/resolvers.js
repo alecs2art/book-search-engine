099f0acf281873a60533fa17c89129ffbce1ab9d
unknown <alecs2art@gmail.com>
2021-10-12 18:38:05 -0700

    created schemas folder with files

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Thought } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+
+      if (context.user) {
+
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password");
+
+        console.log(userData);
+        return userData;
+      }
+
+      throw new AuthenticationError('please log in');
+    },
+  },
+
+  Mutation: {
+
+    login: async (parent, { email, password }) => {
+
+      const user = await User.findOne({
+        $or: [{ email: email }],
+      });
+
+      if (!user) {
+        throw new AuthenticationError('no user');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('wrong password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, args) => {
+
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, args, context) => {
+
+      const bookSave = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $push: { savedBooks: [args.book] } },
+        { new: true }
+      );
+      return bookSave;    
+    
+    },
+
+    removeBook: async (parent, args, context) => {
+
+      const bookRemove = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: {bookId: args.bookId} } },
+        { new: true }
+      );
+      return bookRemove;
+    },
+  },
+};
+
+module.exports = resolvers;
